27371515f9ca2c6a103a4a2c9faba588c0990b2c
tusharawat01 <tushar0402tr@gmail.com>
2024-07-11 03:04:48 +0530

    User details Route or Get Data From Token

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,21 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import {NextRequest, NextResponse} from "next/server";
+import {getDataFromToken} from "@/helpers/getDataFromToken"
+
+connect();
+
+export async function GET(request: NextRequest) {
+    //extract data from token
+    const userId = await getDataFromToken(request)
+    const user = await User.findOne({_id: userId}).select("-password");
+
+    if(!user){
+        return NextResponse.json({error: "Useer not found"}, {status: 400});
+    }
+
+    return NextResponse.json({
+        message: "User Found",
+        data: user
+    })
+}
